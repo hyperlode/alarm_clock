6f2252fcc800c7763c13323cba519feba087593a
lode <lodeameije@gmail.com>
2022-09-06 22:38:45 -0700

    Enable dark night mode

--- arduino_uno_digital_clock/arduino_uno_digital_clock.ino
+++ arduino_uno_digital_clock/arduino_uno_digital_clock.ino
@@ -25,8 +25,8 @@
 #define PIN_DISPLAY_SEGMENT_DP 4
 #define PIN_DISPLAY_SEGMENT_G 7 // swapped!
 
-#define PIN_BUTTON_TIME_UP A3
-#define PIN_BUTTON_TIME_DOWN A2
+#define PIN_button_up A3
+#define PIN_button_down A2
 #define PIN_BUTTON_MENU A1
 #define TIME_UPDATE_DELAY 1000
 #define TIME_HALF_BLINK_PERIOD_MILLIS 250
@@ -36,8 +36,8 @@ DisplayManagement visualsManager;
 LedMultiplexer5x8 ledDisplay;
 GravityRtc rtc;     //RTC Initialization
 
-Button button_time_up;
-Button button_time_down;
+Button button_up;
+Button button_down;
 Button button_menu;
 
 long nextTimeUpdateMillis;
@@ -48,20 +48,28 @@ bool blinker;
 enum Time_type :uint8_t 
 {
     hours=0,
-    minutes,
-    seconds
+    minutes=1,
+    seconds=2
 };
 
 enum Clock_state : uint8_t
 {
   state_display_time = 0,
-  state_set_time_hours,
-  state_set_time_minutes,
-  state_set_time_seconds
-
+  state_set_time,
+  state_night_mode
 };
-
 Clock_state clock_state;
+
+enum Set_time_state: uint8_t 
+{
+  state_set_time_init=0,
+  state_set_time_hours=1,
+  state_set_time_minutes=2,
+  state_set_time_seconds=3,
+  state_set_time_end=4
+};
+Set_time_state set_time_state;
+
 
 void setup() {
 
@@ -76,8 +84,8 @@ void setup() {
   //Set the RTC time manually
   //rtc.adjustRtc(2017,6,19,1,12,7,0);  //Set time: 2017/6/19, Monday, 12:07:00
 
-  button_time_up.init(PIN_BUTTON_TIME_UP);
-  button_time_down.init(PIN_BUTTON_TIME_DOWN);
+  button_up.init(PIN_button_up);
+  button_down.init(PIN_button_down);
   button_menu.init(PIN_BUTTON_MENU);
 
   ledDisplay.Begin(DISPLAY_IS_COMMON_ANODE, PIN_DISPLAY_DIGIT_0, PIN_DISPLAY_DIGIT_1, PIN_DISPLAY_DIGIT_2, PIN_DISPLAY_DIGIT_3, PIN_DISPLAY_DIGIT_BUTTON_LIGHTS, PIN_DISPLAY_SEGMENT_A, PIN_DISPLAY_SEGMENT_B, PIN_DISPLAY_SEGMENT_C, PIN_DISPLAY_SEGMENT_D, PIN_DISPLAY_SEGMENT_E, PIN_DISPLAY_SEGMENT_F, PIN_DISPLAY_SEGMENT_G, PIN_DISPLAY_SEGMENT_DP);
@@ -91,7 +99,7 @@ void setup() {
 
 void cycleBrightness(bool init) {
 
-  uint8_t brightness_settings [] = {1, 10, 80, 254, 0}; // do not use 255, it creates an after glow once enabled. (TODO: why?!) zero is dark. but, maybe you want that... e.g. alarm active without display showing.
+  uint8_t brightness_settings [] = {0, 1, 10, 80, 254}; // do not use 255, it creates an after glow once enabled. (TODO: why?!) zero is dark. but, maybe you want that... e.g. alarm active without display showing.
 
   //#define CYCLING_GOES_BRIGHTER
 
@@ -114,11 +122,19 @@ void cycleBrightness(bool init) {
     brightness = DEFAULT_BRIGHTNESS;
   }
 
-  //ledDisplay.setBrightness(brightness*brightness*brightness*brightness, false);
-  ledDisplay.setBrightness(brightness_settings[brightness], false);
+  
+
+  // enter night mode screen when zero visibility.
+  if (brightness == 0){
+    Serial.println("dark mode enter.");
+    clock_state = state_night_mode;
+    brightness = 4;
+  }
+    ledDisplay.setBrightness(brightness_settings[brightness], false);
+  
 }
 
-void display_time() {
+void display_time_state_refresh() {
 
   if (millis() > nextTimeUpdateMillis ) {
     nextTimeUpdateMillis = millis() + TIME_UPDATE_DELAY;
@@ -132,12 +148,19 @@ void display_time() {
     }
   }
 
-  if (button_time_up.getEdgeDown()) {
+  if (button_menu.getEdgeDown()) {
+    // clock_state = static_cast<Clock_state>(static_cast<int>(clock_state) + 1);;
+    clock_state = state_set_time;
+    Serial.println("move away from time display state");
+        
+  }
+
+  if (button_up.getEdgeDown()) {
 
     //    visualsManager.setDecimalPointsToDisplay(0xFF);
     nextTimeUpdateMillis = millis(); // make sure to refresh display
 
-    if (button_time_up.getToggleValue()) {
+    if (button_up.getToggleValue()) {
       Serial.println("seconds minute ON");
       displayHourMinutesElseMinutesSeconds = false;
 
@@ -147,7 +170,7 @@ void display_time() {
     }
   }
 
-  if (button_time_down.getEdgeDown()) {
+  if (button_down.getEdgeDown()) {
     cycleBrightness(false);
   }
 }
@@ -156,7 +179,10 @@ void divider_colon_to_display() {
   // will blink with a two second period
   rtc.read();
   visualsManager.setDecimalPointToDisplay(rtc.second % 2, 1);
-  
+  visualsManager.setDecimalPointToDisplay(true, 0);
+  visualsManager.setDecimalPointToDisplay(true, 2);
+  visualsManager.setDecimalPointToDisplay(true, 3);
+          
 }
 void seconds_to_display() {
   int16_t timeAsNumber;
@@ -178,24 +204,68 @@ void hour_minutes_to_display() {
   }
 }
 
+void set_time_state_refresh(){
+    switch(set_time_state){
+        case state_set_time_init:
+        {
+            set_time_state = state_set_time_hours;
+            // Serial.println("at time state: INIT");
+        }
+        break;
+        case state_set_time_hours:
+        {
+            set_time(hours);
+        }
+        break;
+        case state_set_time_minutes:
+        {
+            set_time(minutes);
+        }
+        break;
+        case state_set_time_seconds:
+        {
+            set_time(seconds);
+        }
+        break;
+        case state_set_time_end:
+        {
+            set_time_state = state_set_time_init;
+            clock_state = state_display_time;
+            // Serial.println("at time state: END");
+        }
+        break;
+        default:
+        {
+        }
+        break;
+    }
+    if (button_menu.getEdgeDown()) {
+        // cycle trough states in sequence
+        set_time_state = static_cast<Set_time_state>(static_cast<int>(set_time_state) + 1);;
+        // clock_state = state_set_time;
+        // set_time_state = state_set_time_hours;
+        
+    }
+}
+
 void set_time(Time_type t) {
-
-
-  if (button_time_up.getEdgeDown() || button_time_down.getEdgeDown()) {
+  if (button_up.getEdgeDown() || button_down.getEdgeDown()) {
     rtc.read();
 
     if (t == hours){
-        nextStepRotate(&rtc.hour, button_time_up.getEdgeDown(), 0, 23);
+        nextStepRotate(&rtc.hour, button_up.getEdgeDown(), 0, 23);
     }else if (t == minutes){
-        nextStepRotate(&rtc.minute, button_time_up.getEdgeDown(), 0, 59);
+        nextStepRotate(&rtc.minute, button_up.getEdgeDown(), 0, 59);
     }else if (t == seconds){
-        //nextStepRotate(&rtc.minute, button_time_up.getEdgeDown(), 0, 59);
+        //nextStepRotate(&rtc.minute, button_up.getEdgeDown(), 0, 59);
         rtc.second = 0;
     }
     rtc.adjustRtc(rtc.year, rtc.month, rtc.day, rtc.week, rtc.hour, rtc.minute, rtc.second);
   }
 
   if (millis() > nextTimeUpdateMillis ) {
+    
+            
     nextTimeUpdateMillis = millis() + TIME_HALF_BLINK_PERIOD_MILLIS;
 
     divider_colon_to_display();
@@ -224,47 +294,62 @@ void set_time(Time_type t) {
   }
 }
 
+void display_on_touch_state_refresh(){
+
+    if (button_down.getEdgeDown()){
+        Serial.println("back to state display time");
+        clock_state = state_display_time;
+    
+    }else if (button_menu.getEdgeDown() || button_up.getEdgeDown()){
+        ledDisplay.setBrightness(100, false);
+    }else if (button_menu.getEdgeUp() || button_up.getEdgeUp()){
+        ledDisplay.setBrightness(0, false);
+    } else if (!(button_menu.getValue()) || !(button_up.getValue())){
+        divider_colon_to_display();
+        hour_minutes_to_display();
+    }else{
+        visualsManager.setBlankDisplay();
+    } 
+}
+
 void refresh_clock_state() {
   switch (clock_state)
   {
     case state_display_time:
-      {
-        display_time();
-      }
-      break;
-    case state_set_time_hours:
-      {
-        set_time(hours);
-      }
-      break;
-    case state_set_time_minutes:
-      {
-        set_time(minutes);
-      }
-      break;
-    case state_set_time_seconds:
-      {
-        set_time(seconds);
-      }
-      break;
+    {
+        display_time_state_refresh();
+    }
+    break;
+    case state_set_time:
+    {
+        set_time_state_refresh();
+    }
+    break;
+    case state_night_mode:
+    {
+        display_on_touch_state_refresh();
+    }
+    break;
     default:
+    {
       clock_state = state_display_time;
-      break;
+    }
+    break;
   }
 }
 
 void loop() {
 
-  button_time_up.refresh();
-  button_time_down.refresh();
+  button_up.refresh();
+  button_down.refresh();
   button_menu.refresh();
 
   refresh_clock_state();
 
-  if (button_menu.getEdgeDown()) {
-    clock_state = static_cast<Clock_state>(static_cast<int>(clock_state) + 1);;
-    Serial.println(clock_state);
-  }
+//   if (button_menu.getEdgeDown()) {
+//     clock_state = static_cast<Clock_state>(static_cast<int>(clock_state) + 1);;
+//     Serial.println(clock_state);
+//   }
 
   visualsManager.refresh();
   ledDisplay.refresh();
